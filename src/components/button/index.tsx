328ab2348f163485920e1f8039ea8bae3fdb873a
Bryan Weaver <weaverb9@gmail.com>
2022-05-06 12:19:39 -0500

    add spin while loading

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,15 +1,18 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch } from "../../store";
-import { fetchSpy } from "../../store/appSlice";
+import { fetchSpy, getIsLoading } from "../../store/appSlice";
 
 const ActionButton = () => {
   const dispatch: AppDispatch = useDispatch();
+  const loading = useSelector(getIsLoading);
+
   return (
     <button
       className="btn btn-lg btn-dark mt-5"
       onClick={() => dispatch(fetchSpy())}
     >
-      Next Asset
+      {loading && <span className="spinner-border spinner-border-sm"></span>}
+      {loading ? " Searching..." : "Next Asset"}
     </button>
   );
 };
